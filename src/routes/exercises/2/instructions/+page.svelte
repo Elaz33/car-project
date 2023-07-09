9973fb553fbdbbea98405d16be1ed361516bfed4
jdr <admin@firecoding.com>
2023-07-09 13:23:23 -0400

    add exercise 2

---
+++ src/routes/exercises/2/instructions/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+
+
+
+</script>
+<!------------------------------------------------------------>
+
+<h2>Exercise 2</h2>
+<ul>
+  <li>Set up a figma account if you don't already have one</li>
+  <li>Replicate <a href="https://www.figma.com/file/MtNh7uCnKuVQaZmm99NqxZ/Elazar-Practice?type=design&node-id=0%3A1&mode=design&t=ImB26Kt0nXXTW1x4-1" target="_blank">this page</a> in a svelte file</li>
+  <li>Be PIXEL PERFECT (get absolutely everything EXACTLY the same: colors, sizes, features, even the exact Google font, etc)</li>
+  <li>Your submitted answer should be here: <a href="/exercises/2/my-answer" target="_blank">/exercises/2/my-answer</a></li>
+</ul>
+
+<!------------------------------------------------------------>
+<!--<style lang="scss"></style>-->
+
