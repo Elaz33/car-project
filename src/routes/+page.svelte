283c3b0615034a3e94df31d7a2fcb4be09ab8319
Elazar <elazrosenfeld@gmail.com>
2023-07-09 23:21:46 -0400

    blue boxes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,4 @@
 <h1>Welcome to Elazar's Practice project</h1>
 <p><a href="/exercises/1/instructions">Exercise 1</a></p>
 <p><a href="/exercises/2/instructions">Exercise 2</a></p>
+<p><a href="/exercises/1/my-answer">my answer</a></p>
--- src/routes/exercises/1/my-answer/+page.svelte
+++ src/routes/exercises/1/my-answer/+page.svelte
@@ -1,12 +1,34 @@
 <script lang="ts">
-
-
-
+    let incrementor = 0;
+    let numThree = 1000;
+    let threes: number[] = [...Array(numThree)].map(() => {
+        return (incrementor += 3);
+    });
 </script>
+
 <!------------------------------------------------------------>
 
-<h2>Hatzlacha!</h2>
+<h2>Numbers:</h2>
+<div class="the-box">
+    {#each threes as three}
+        <div class="items">{three}</div>
+    {/each}
+</div>
 
 <!------------------------------------------------------------>
 <!--<style lang="scss"></style>-->
-
+<style>
+    .the-box {
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .items {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        flex: 1;
+        background-color: blue;
+        margin: 5px;
+        padding: 10px;
+    }
+</style>
