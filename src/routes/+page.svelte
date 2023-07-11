025f9c10676c063dc14554d9db837cff598e57fe
jdr <admin@firecoding.com>
2023-07-11 16:28:10 -0400

    fix exercise 1 answer link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,3 @@
 <h1>Welcome to Elazar's Practice project</h1>
 <p><a href="/exercises/1/instructions">Exercise 1</a></p>
 <p><a href="/exercises/2/instructions">Exercise 2</a></p>
-<p><a href="/exercises/1/my-answer">my answer</a></p>
