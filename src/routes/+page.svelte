ada2816dc10cd51b737da4e451e9c5af0d00b200
Elazar <elazrosenfeld@gmail.com>
2023-07-12 08:34:40 -0400

    exercise 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <h1>Welcome to Elazar's Practice project</h1>
 <p><a href="/exercises/1/instructions">Exercise 1</a></p>
+<p><a href="/exercises/1/my-answer">my answer</a></p>
 <p><a href="/exercises/2/instructions">Exercise 2</a></p>
+<p><a href="exercises/2/my-answer">my answer</a></p>
 <p><a href="/exercises/3/instructions">Exercise 3</a></p>
