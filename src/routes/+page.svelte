d1b43455c282a090dd3c84bcfa4974c00ff9d8c9
jdr <admin@firecoding.com>
2023-07-18 15:34:04 -0400

    set up exercise 5

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,4 @@
 <p><a href="/exercises/2/instructions">Exercise 2</a></p>
 <p><a href="/exercises/3/instructions">Exercise 3</a></p>
 <p><a href="/exercises/4/instructions">Exercise 4</a></p>
+<p><a href="/exercises/5/instructions">Exercise 5</a></p>
