3b9ed6f95d3292fe3ab4c5415cec6735f0a52856
jdr <admin@firecoding.com>
2023-07-18 14:47:48 -0400

    set up exercise 4, add basic-store-example

---
+++ src/routes/exercises/4/basic-store-example/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+
+  import {magicNumberStore} from '$lib/stores/magic-number.store';
+
+
+function reset() {
+   magicNumberStore.set(0);
+ }
+
+</script>
+<!------------------------------------------------------------>
+
+<div class="parent">
+  <div><button on:click={magicNumberStore.decrement}>-1</button></div>
+  <div class="magic-number">{$magicNumberStore}</div>
+  <div><button on:click={magicNumberStore.increment}>+1</button></div>
+</div>
+
+<button on:click={reset}>RESET</button>
+
+<!------------------------------------------------------------>
+<style>
+
+  .parent {
+    display: flex;
+    align-items: center;
+  }
+
+  .magic-number {
+    margin: 30px;
+    font-size: 36px;
+  }
+
+  button {
+    height: 40px;
+  }
+
+</style>
+
