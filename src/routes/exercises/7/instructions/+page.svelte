f652dda4f58866e01fc6ba909c81144ef179d7fb
jdr <admin@firecoding.com>
2023-08-04 15:28:47 -0400

    add exercise 7a, 7b, and solution for 7a

---
+++ src/routes/exercises/7/instructions/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+
+
+
+</script>
+<!------------------------------------------------------------>
+
+<h2>Exercise 7</h2>
+
+<h4>Part A</h4>
+<ul>
+  <li>Output every number between 1 and 100 which is divisible by 3</li>
+  <li>Create a page to output your results, one value per line, here: <a href="/exercises/7/my-answer/a">/exercises/7/my-answer/a</a></li>
+</ul>
+
+<h4>Part B</h4>
+<ul>
+  <li>Output every number between 1 and 50 multiplied times PI.  The output should always display exactly 2 digits of precision.</li>
+  <li>For example, here are the first 3 values to output: 3.14, 6.28, 9.42</li>
+  <li>Create a page to output your results, one value per line, here: <a href="/exercises/7/my-answer/b">/exercises/7/my-answer/b</a></li>
+</ul>
+
+<!------------------------------------------------------------>
+<!--<style lang="scss"></style>-->
+
---
+++ src/routes/exercises/7/solution/a/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+
+
+  const numbers:number[] = [];
+
+  // populate from 1..100
+  for (let i=1; i <= 100; i++) {
+    numbers.push(i);
+  }
+
+  // get all numbers which are divisible by 3
+  const numbersDivided = numbers.filter(n => n % 3 === 0);
+
+
+</script>
+<!------------------------------------------------------------>
+
+{#each numbersDivided as num}
+  <div>{num}</div>
+{/each}
+
+
+<!------------------------------------------------------------>
+<!--<style lang="scss"></style>-->
+
