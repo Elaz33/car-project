73b603d2edc95514ebf430da6eb17af93e70187d
jdr <admin@firecoding.com>
2023-08-09 22:37:22 -0400

    changes as of Aug-9-2023

--- src/routes/exercises/7/my-answer/a/+page.svelte
+++ src/routes/exercises/7/my-answer/a/+page.svelte
@@ -1,12 +1,14 @@
 <script>
     let triples = [];
-    for (let i = 0; i <= 100; i++) {
-        if (i % 3 == 0) {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0) {
             triples.push(i);
         }
     }
 </script>
 
-{#each triples as threes}
-    <div class="threes">{threes}</div>
-{/each}
+<div class="threes">
+    {#each triples as three}
+        <div class="three">{three}</div>
+    {/each}
+</div>
--- src/routes/exercises/7/my-answer/b/+page.svelte
+++ src/routes/exercises/7/my-answer/b/+page.svelte
@@ -1,9 +1,8 @@
 <script>
     let pies = [];
-    let copy;
-    for (let i = 0; i <= 50; i++) {
-        copy = i * Math.PI;
-        pies.push(copy.toPrecision(3));
+    for (let i = 1; i <= 50; i++) {
+        const piMultiple = i * Math.PI;
+        pies.push(piMultiple.toPrecision(3));
     }
     console.log(pies);
 </script>
